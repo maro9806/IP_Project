3337d7ae8d2a93060f446d820dc44d5a2cd16714
hyunjun-17 <wlfkem159@gmail.com>
2024-10-21 17:41:39 +0900

    24.10.21 17:

--- src/main/java/com/ip_project/config/WebConfig.java
+++ src/main/java/com/ip_project/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ip_project.config;
 
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
+        registry.addResourceHandler("/resources/**")
                 .addResourceLocations("classpath:/static/");
     }
 }
