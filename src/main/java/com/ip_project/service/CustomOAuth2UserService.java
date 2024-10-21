a20c06a1523899283e10d5aaede83ff28c6f6d49
hyunjun-17 <wlfkem159@gmail.com>
2024-10-21 14:29:12 +0900

    24.10.21 11:

--- src/main/java/com/ip_project/service/CustomOAuth2UserService.java
+++ src/main/java/com/ip_project/service/CustomOAuth2UserService.java
@@ -24,9 +24,6 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
-        // 여기서 사용자 정보를 데이터베이스에 저장하거나 업데이트하는 로직을 구현합니다.
-
-
         return new DefaultOAuth2User(
                 Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
                 attributes.getAttributes(),
