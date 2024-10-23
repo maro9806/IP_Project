525a98991f7d3469ee902953e4bd7e74dba406fc
hyunjun-17 <wlfkem159@gmail.com>
2024-10-23 14:07:58 +0900

    24.10.22 17:

---
+++ src/main/java/com/ip_project/entity/InterviewStatus.java
@@ -0,0 +1,9 @@
+package com.ip_project.entity;
+
+public enum InterviewStatus {
+    WAITING,    // 대기중
+    IN_PROGRESS,// 진행중
+    SUBMITTED,  // 제출됨
+    COMPLETED,  // 완료됨
+    CANCELLED   // 취소됨
+}
