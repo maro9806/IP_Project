2bdcead9ee91bf3adcff45ff9ac5baef6287aa8a
hyunjun-17 <wlfkem159@gmail.com>
2024-11-04 10:57:48 +0900

    24.11.04 10:

--- src/main/java/com/ip_project/config/NewsApiConfig.java
+++ src/main/java/com/ip_project/config/NewsApiConfig.java
@@ -11,4 +11,5 @@ import org.springframework.context.annotation.Configuration;
 @Setter
 public class NewsApiConfig {
     private String key;
+
 }
--- src/main/java/com/ip_project/controller/CompanyController.java
+++ src/main/java/com/ip_project/controller/CompanyController.java
@@ -1,38 +1,42 @@
 package com.ip_project.controller;
 
 import com.ip_project.entity.Company;
+import com.ip_project.entity.Swot;
 import com.ip_project.service.CompanyService;
-import lombok.RequiredArgsConstructor;
+import com.ip_project.service.SwotService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
 @RequestMapping("/company")
-@RequiredArgsConstructor  // 생성자 주입으로 변경
+@Controller
 public class CompanyController {
 
-    private final CompanyService companyService;  // 하나의 서비스 인스턴스만 사용
+    @Autowired
+    public CompanyService service;
+    @Autowired
+    private CompanyService companyService;
+    @Autowired
+    private SwotService swotService;
 
     @RequestMapping("/corpmain")
     public String list(Model model) {
-        companyService.list(model);
+        service.list(model);
         return "company/corpmain";
     }
 
     @GetMapping("/corp")
-    public String corp(@RequestParam(value = "companyIdx", required = false) Long companyIdx, Model model) {
-        Company board;
-        if (companyIdx != null) {
-            // companyIdx가 제공된 경우 해당 회사 정보를 조회
-            board = companyService.get(companyIdx);
-        } else {
-            // companyIdx가 없는 경우 새로운 Company 객체 생성
-            board = new Company();
-        }
+    public String corp(@RequestParam("companyIdx") Long companyIdx, Model model) {
+        Company board = companyService.get(companyIdx);
         model.addAttribute("board", board);
+        Swot swot = swotService.findByCompanyIdx(companyIdx);
+        model.addAttribute("swot", swot);
+
         return "company/corp";
     }
+
+
 }
